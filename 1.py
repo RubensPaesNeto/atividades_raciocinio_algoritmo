25c849a0e26cb78d1d048ccccdc61dde303b4096
Rubens Paes Neto <rubenspaesneto@gmail.com>
2026-03-31 11:17:27 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Informe o valor 1: "))
+valor2 = int(input("Informe o valor 2: "))
+if valor1 > valor2:
+    print("Valor 1 é maior que Valor 2")
+elif valor1 == valor2:
+    print("Valores iguais")
+else:
+    print("Vslor 2 é maior que Valor 1")
